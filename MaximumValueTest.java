e83ce4cd8f7d47e59dc8879f50ce0c410418cc42
vishalj1412 <vishaljagtap753@gmail.com>
2021-10-13 13:25:09 +0530

    find maximum value among String value

--- MaximumValueTest.java
+++ MaximumValueTest.java
@@ -8,6 +8,8 @@ public class MaximumValueTest {
         MaximumValue maxvalue=new MaximumValue();
         //integer three value and check max value
       int value= maxvalue.maximum(3,10,5);
+      String string =maxvalue.maximum("vishal","kunal","raj");
      System.out.println(value);
+     System.out.println(""+string);
     }
 }
