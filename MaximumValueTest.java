681e6182d211eb68e377dfa0b2f33fc5a49e8703
vishalj1412 <89152210+vishalj1412@users.noreply.github.com>
2021-10-13 16:41:21 +0530

    Add: find max value amoung diff data type

--- MaximumValueTest.java
+++ MaximumValueTest.java
@@ -8,6 +8,14 @@ public class MaximumValueTest {
         MaximumValue maxvalue=new MaximumValue();
         //integer three value and check max value
       int value= maxvalue.maximum(3,10,5);
+      String string =maxvalue.maximum("vishal","kunal","raj");
      System.out.println(value);
+     System.out.println(""+string);
+    }
+    @Test
+    public void FolatingTest(){
+        MaximumValue maxval=new MaximumValue();
+        float val= maxval.maximum(3.1f,1.23,5.4f);
+        System.out.println(""+val);
     }
 }
