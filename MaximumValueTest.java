7d7b9d391ade15e87e88aab1c353140b01866418
vishalj1412 <89152210+vishalj1412@users.noreply.github.com>
2021-10-13 16:33:19 +0530

    Add: Find maximum amoung three string values

--- MaximumValueTest.java
+++ MaximumValueTest.java
@@ -8,6 +8,7 @@ public class MaximumValueTest {
         MaximumValue maxvalue=new MaximumValue();
         //integer three value and check max value
       int value= maxvalue.maximum(3,10,5);
+        //find maximum for passing String
       String string =maxvalue.maximum("vishal","kunal","raj");
      System.out.println(value);
      System.out.println(""+string);
